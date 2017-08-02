07228f2437bd51a492154e34b9877c087ba98486
Christopher Ryder <cryder9898@gmail.com>
2017-08-02 10:14:32 -0400

    added working line graph

--- js/script.js
+++ js/script.js
@@ -1,27 +1,66 @@
 const dataURL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-const svg = d3.select('svg');
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
 
-function render(data) {
-  console.log(data);
-  // bind data
-  let circles = svg.selectAll('circles').data(data);
-  // enter
-  circles.enter().append('circle')
-    .attr('r', 10);
-  // update
-  circles
-    .attr('cx', function(d) {return d})
-    .attr('cy', function(d) {return d});
-  // exit
-  circles.exit().remove();
-}
+// Parse the date / time
+var parseDate = d3.time.format("%Y-%m-%d").parse;
 
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+    .orient("bottom").ticks(5);
+
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(5);
+
+// Define the line
+var valueline = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.gdp); });
+
+// Adds the svg canvas
+var svg = d3.select("body")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
 d3.json(dataURL, function(error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data.data);
-    render(data.data);
-  }
+    data = data.data.map(function(d) {
+        let obj = {};
+        obj.date = parseDate(d[0]);
+        //console.log(parseDate(d[0]));
+        obj.gdp = +d[1];
+        return obj;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.gdp; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
 });
